a5a9403b4f02a414ad8c4c03d6469f3a3c03eee3
AnaisUrlichs <33576047+AnaisUrlichs@users.noreply.github.com>
2018-12-19 20:15:54 +0000

    Create more-objects.js

---
+++ more-objects.js
@@ -0,0 +1,94 @@
+//1st exercise
+
+const robot = {
+  model: '1E78V2',
+  energyLevel: 100,
+  provideInfo() { 
+    return `I am ${this.model} and my current energy level is ${this.energyLevel}.`
+  }
+};
+
+console.log(robot.provideInfo());
+
+
+//2nd exercise
+
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  get energyLevel(){
+    if(typeof this._energyLevel === 'number') {
+      return 'My current energy level is ' + this._energyLevel
+    } else {
+      return "System malfunction: cannot retrieve energy level"
+    }
+  }
+};
+
+console.log(robot.energyLevel);
+
+
+
+//3rd Exercise
+
+const robot = {
+  _model: '1E78V2',
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors(){
+    if(typeof this._numOfSensors === 'number'){
+      return this._numOfSensors;
+    } else {
+      return 'Sensors are currently down.'
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === 'number' && num >= 0){
+      this._numOfSensors = num;
+    } else {
+      console.log('Pass in a number that is greater than or equal to 0')
+    }   
+  } 
+};
+
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors);
+
+
+//4th exercise
+
+const robotFactory = (model, mobile) => {
+  return {
+  model: model,
+  mobile: mobile,
+  beep() {
+    console.log('Beep Boop');
+  }
+  }
+};
+
+const tinCan = robotFactory('P-500', true);
+
+tinCan.beep();
+
+
+//5th exercise
+
+const robot = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+const robotEntries = Object.entries(robot)
+console.log(robotEntries);
+
+const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot);
+
+console.log(newRobot);
